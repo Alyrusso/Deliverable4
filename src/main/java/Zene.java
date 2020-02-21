c2026e1c5d13770f424b43e3fc0116e6fe51038f
Spencer Hedrick <shedric1@asu.edu>
2020-02-20 17:55:40 -0800

    add arg checking and member variables for jdbc connection handling

--- src/main/java/Zene.java
+++ src/main/java/Zene.java
@@ -1,5 +1,6 @@
 package main.java;
 
+import java.sql.*;
 import java.util.LinkedHashSet;
 import java.util.Scanner;
 import java.util.Set;
@@ -8,21 +9,34 @@ import java.util.Set;
  * Entry point and menu handler for group 17 music database application
  */
 public class Zene {
+    private static Connection conn;
+    private static Statement statement;
+    private static ResultSet rs;
+    private static PreparedStatement pStatement;
+
     private static Set<String> menuOptions;
     private static Scanner in;
+
+    private static String url;
+    private static String username;
+    private static String password;
+    private static String driver;
 
     public static void main(String[] args) {
         //initialize member objects
         in = new Scanner(System.in);
+        verifyArgs(args);
         menuOptions = new LinkedHashSet<>();
 
         //add all the various menu options.
         //use "\033[4m" to start underline. use "\033[0m" to return to normal
         //remember to add a case to processOption() for the underlined char!
-        menuOptions.add("\033[4ma\033[0mrtist search");
-        menuOptions.add("\033[4mt\033[0mitle search");
-        menuOptions.add("add \033[4mn\033[0mew audio file");
-        menuOptions.add("add new alb\033[4mu\033[0mm");
+        menuOptions.add("a: artist search");
+        menuOptions.add("t: title search");
+        menuOptions.add("n: add new audio file");
+        menuOptions.add("u: add new album");
+
+        connectDB();
 
         //menu loop
         char lastOption = '\0';
@@ -31,6 +45,8 @@ public class Zene {
             lastOption = Character.toLowerCase(in.next().charAt(0));
             if (lastOption != 'q') processOption(lastOption);
         }
+
+        disconnectDB();
     }
 
     //switch for all the various options that may be called
@@ -71,10 +87,64 @@ public class Zene {
 
     //helper method for printing all menu options
     private static void printOptions() {
-        int count = 0;
-        for (String s: menuOptions) {
-            System.out.printf("\t%d: %s\n",++count, s);
+        for (String s: menuOptions)
+            System.out.println("\t" + s);
+        System.out.print("Select menu option by preceding character (or q to exit): ");
+    }
+
+    //called from main, verifies args are proper
+    private static void verifyArgs(String[] args) {
+        //check for sufficient args, prompt for user input otherwise
+        if (args.length < 4) {
+            System.out.println("Please enter the database URL (e.g. jdbc:mysql://localhost:3306/world)");
+            url = in.next();
+            System.out.println("Please enter your database username: ");
+            username = in.next();
+            System.out.println("Please enter your database password: ");
+            password = in.next();
+            System.out.println("Please enter the driver you wish to use (e.g. com.mysql.cj.jdbc.Driver)");
+            driver = in.next();
+        } else {
+            url = args[0];
+            username = args[1];
+            password = args[2];
+            driver = args[3];
         }
-        System.out.print("Select menu option by underlined character (or q to exit): ");
+
+        //check for proper URI protocol
+        if (!url.toLowerCase().contains("jdbc:"))
+            throw new IllegalArgumentException("<url> must start with \"jdbc:\"");
+    }
+
+    //disconnects all DB resources if initialized then exits the program
+    private static void disconnectDB() {
+        System.out.print("closing db connection...");
+        try {
+            //close all DB resources
+            if (rs != null) rs.close();
+            if (pStatement != null) pStatement.close();
+            if (statement != null) statement.close();
+            if (conn != null) conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("closed!");
+        System.exit(0);
+    }
+
+    private static void connectDB() {
+        System.out.print("connecting to db...");
+        try {
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, username, password);
+            //conn.setAutoCommit(false); //do we want autocommit on/off?
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error: driver not found");
+            disconnectDB();
+        } catch (SQLException e) {
+            System.out.println("Error: could not connect to database. Wrong url or login info?");
+            disconnectDB();
+        }
+        System.out.println("connected!");
     }
 }
